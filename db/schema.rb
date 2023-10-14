29d6546fdf389d77dcda68d5e2abcab1366e1fd0
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-10-14 13:51:53 -0600

    fix test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_154254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
