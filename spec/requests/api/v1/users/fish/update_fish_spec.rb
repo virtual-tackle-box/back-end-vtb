8ff921d800fe111372238b6525313b47a3393fb8
Wil Fady <wrfady@gmail.com>
2023-10-15 12:23:54 -0400

    Adds sad path testing for fish update

--- spec/requests/api/v1/users/fish/update_fish_spec.rb
+++ spec/requests/api/v1/users/fish/update_fish_spec.rb
@@ -33,5 +33,30 @@ RSpec.describe "Fish", type: :request do
       expect(fish_data[:data][:attributes][:weight]).to eq(updated_fish.weight)
       expect(fish_data[:data][:attributes][:length]).to eq(updated_fish.length)
     end
+
+    it "cant update fish w/ invalid params" do
+      user = create(:user)
+      fish = user.fish.create(species: "Largemouth Bass", weight: 4.4, length: 18.5)
+
+      fish_params = {
+        "fish": {
+          "species": "",
+          "weight": 4.4,
+          "length": ""
+        }
+      }
+
+      headers = { "CONTENT_TYPE" => "application/json" } 
+      patch "/api/v1/users/#{user.id}/fish/#{fish.id}", headers: headers, params: JSON.generate(fish_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      error_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_data).to be_a(Hash)
+      expect(error_data).to have_key(:error)
+      expect(error_data[:error]).to eq("Validation failed: Species can't be blank, Length can't be blank")
+    end
   end
 end
