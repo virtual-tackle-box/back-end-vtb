2bd96d9309a6f3e190af218dc4c597ea9471820d
Wil Fady <wrfady@gmail.com>
2023-10-15 12:50:20 -0400

    Adds sad path test for fish delete action

--- spec/requests/api/v1/users/fish/delete_fish_spec.rb
+++ spec/requests/api/v1/users/fish/delete_fish_spec.rb
@@ -14,5 +14,21 @@ RSpec.describe "Fish", type: :request do
 
       expect{ Fish.find(fish.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
+
+    it "cannot delete a fish that does not exist" do 
+      user = create(:user)
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+      delete "/api/v1/users/#{user.id}/fish/1", headers: headers
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      error_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_data).to be_a(Hash)
+      expect(error_data).to have_key(:error)
+      expect(error_data[:error]).to eq("No Fish with that ID could be found")
+    end
   end
 end
