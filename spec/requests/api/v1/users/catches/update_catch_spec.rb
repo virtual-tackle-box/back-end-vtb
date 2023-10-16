a3300bc60e9f137d668a4481b9d0bb860569bb5f
Wil Fady <wrfady@gmail.com>
2023-10-16 14:31:45 -0400

    Refactor update request spec from fish to catches

---
+++ spec/requests/api/v1/users/catches/update_catch_spec.rb
@@ -0,0 +1,95 @@
+require "rails_helper"
+
+RSpec.describe "Catch", type: :request do
+  describe "PATCH /api/v1/users/:user_id/catches/:id" do
+    it "can update a catch w/ valid params" do
+      user = create(:user)
+      catch = user.catches.create(
+        species: "Largemouth Bass",
+        weight: 4.4,
+        length: 18.5,
+        spot_name: "Lake A",
+        latitude: 40.7128,
+        longitude: -74.0060,
+        lure: "Spinnerbait",
+        photo_url: "https://example.com/photo.jpg"
+      )
+
+      catch_params = {
+        "catch": {
+          "species": "Pike",
+          "weight": 4.5,
+          "length": 29.1,
+          "spot_name": "Lake D",
+          "latitude": 38.9072,
+          "longitude": -77.0370,
+          "lure": "Topwater",
+          "photo_url": "https://example.com/photo4.jpg"
+        }
+      }
+
+      headers = { "CONTENT_TYPE" => "application/json" } 
+      patch "/api/v1/users/#{user.id}/catches/#{catch.id}", headers: headers, params: JSON.generate(catch_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      updated_catch = Catch.find(catch.id)
+      catch_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(catch_data).to be_a(Hash)
+      expect(catch_data).to have_key(:data)
+      expect(catch_data[:data].keys).to eq([:id, :type, :attributes])
+      expect(catch_data[:data][:id]).to eq(updated_catch.id.to_s)
+      expect(catch_data[:data][:type]).to be_a(String)
+      expect(catch_data[:data][:attributes].keys).to eq([:species, :weight, :length, :spot_name, :latitude, :longitude, :lure, :photo_url])
+      expect(catch_data[:data][:attributes][:species]).to eq(updated_catch.species)
+      expect(catch_data[:data][:attributes][:weight]).to eq(updated_catch.weight)
+      expect(catch_data[:data][:attributes][:length]).to eq(updated_catch.length)
+      expect(catch_data[:data][:attributes][:spot_name]).to eq(updated_catch.spot_name)
+      expect(catch_data[:data][:attributes][:latitude]).to eq(updated_catch.latitude)
+      expect(catch_data[:data][:attributes][:longitude]).to eq(updated_catch.longitude)
+      expect(catch_data[:data][:attributes][:lure]).to eq(updated_catch.lure)
+      expect(catch_data[:data][:attributes][:photo_url]).to eq(updated_catch.photo_url)
+    end
+
+    it "cant update catch w/ invalid params" do
+      user = create(:user)
+      catch = user.catches.create(
+        species: "Largemouth Bass",
+        weight: 4.4,
+        length: 18.5,
+        spot_name: "Lake A",
+        latitude: 40.7128,
+        longitude: -74.0060,
+        lure: "Spinnerbait",
+        photo_url: "https://example.com/photo.jpg"
+      )
+
+      catch_params = {
+        "catch": {
+          "species": "",
+          "weight": 4.5,
+          "length": "",
+          "spot_name": "Lake D",
+          "latitude": 38.9072,
+          "longitude": -77.0370,
+          "lure": "Topwater",
+          "photo_url": "https://example.com/photo4.jpg"
+        }
+      }
+
+      headers = { "CONTENT_TYPE" => "application/json" } 
+      patch "/api/v1/users/#{user.id}/catches/#{catch.id}", headers: headers, params: JSON.generate(catch_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      error_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_data).to be_a(Hash)
+      expect(error_data).to have_key(:error)
+      expect(error_data[:error]).to eq("Validation failed: Species can't be blank, Length can't be blank")
+    end
+  end
+end
