30c77fe43cc255d760592b34cb045d69d50a5a0d
Wil Fady <wrfady@gmail.com>
2023-10-18 19:11:37 -0400

    Adds phone number to factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
     password_confirmation { password }
+    phone_number { Faker::PhoneNumber.cell_phone }
   end
 end
