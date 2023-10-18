3a5d922de195a22199df40fca7487e6bcaf47a46
Wil Fady <wrfady@gmail.com>
2023-10-18 19:11:00 -0400

    adds twilio to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,28 @@
-class Api::V1::UsersController < ApplicationController 
-  def create 
+class Api::V1::UsersController < ApplicationController
+  def create
     @user = User.new(user_params)
+    
     begin 
       @user.save!
+
+      # Check if the user has a phone number and send a welcome message
+      if @user.phone_number.present?
+        twilio_service.send_welcome_message(@user)
+      end
+
       render json: UserSerializer.new(@user), status: 201
     rescue ActiveRecord::RecordInvalid => e 
       render json: { error: e.message }, status: 422 
     end
   end
 
-
   private 
 
   def user_params 
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :phone_number)
   end
 
+  def twilio_service
+    @twilio_service ||= TwilioService.new
+  end
 end
