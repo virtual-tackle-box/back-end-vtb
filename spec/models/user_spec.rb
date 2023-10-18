16c3eb01d51505e3cd5722dcb7b04751125591aa
Wil Fady <wrfady@gmail.com>
2023-10-18 19:00:35 -0400

    Adds phone number to user model and tests for send_welcome_message

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,30 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password_digest }
+    it { should allow_value("+123456789").for(:phone_number) }
+    it { should_not allow_value("invalid_number").for(:phone_number) }
+    it { should allow_value(nil).for(:phone_number) }
+  end
+
+  describe "callbacks" do
+    describe "after_create" do
+      it "sends a welcome message if phone_number is present" do
+        user = build(:user, phone_number: "+123456789")
+        
+        # Assuming you have a proper factory defined for User (using FactoryBot)
+        expect_any_instance_of(TwilioService).to receive(:send_welcome_message).with(user)
+
+        user.save
+      end
+
+      it "does not send a welcome message if phone_number is not present" do
+        user = build(:user, phone_number: nil)
+        
+        # Assuming you have a proper factory defined for User (using FactoryBot)
+        expect_any_instance_of(TwilioService).not_to receive(:send_welcome_message)
+
+        user.save
+      end
+    end
   end
 end
