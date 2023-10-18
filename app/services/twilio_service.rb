51d3fb5cb50e85ab570a2bfac4735666913354e3
Wil Fady <wrfady@gmail.com>
2023-10-18 17:22:47 -0400

    Complete twilio service

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,9 +1,27 @@
 class TwilioService
   def initialize
-    @client = Twilio::REST::Client.new
+    @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
-  def send_welcome_message(phone_number)
-    
+  def send_welcome_message(user)
+    message = @client.messages.create(
+      body: "Welcome to Virtual Tacklebox! Thank you for joining, stay tuned for important fishing updates!",
+      from: twilio_phone_number,
+      to: user.phone_number
+    )
+  end
+
+  private
+
+  def account_sid
+    Rails.application.credentials.twilio[:account_sid]
+  end
+
+  def auth_token
+    Rails.application.credentials.twilio[:auth_token]
+  end
+
+  def twilio_phone_number
+    Rails.application.credentials.twilio[:phone_number]
   end
 end
