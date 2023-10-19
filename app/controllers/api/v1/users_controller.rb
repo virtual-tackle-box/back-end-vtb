a61be797641ac91561739cb5ce9b2d5f29cea77a
Wil Fady <wrfady@gmail.com>
2023-10-19 19:06:58 -0400

    Completes twilio and sidekiq setup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,7 @@ class Api::V1::UsersController < ApplicationController
     
     begin 
       @user.save!
-
-      # Check if the user has a phone number and send a welcome message
-      if @user.phone_number.present?
-        twilio_service.send_welcome_message(@user)
-      end
-
+      TwilioSenderJob.perform_async(@user.id)
       render json: UserSerializer.new(@user), status: 201
     rescue ActiveRecord::RecordInvalid => e 
       render json: { error: e.message }, status: 422 
@@ -20,9 +15,5 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params 
     params.require(:user).permit(:email, :password, :password_confirmation, :phone_number)
-  end
-
-  def twilio_service
-    @twilio_service ||= TwilioService.new
   end
 end
