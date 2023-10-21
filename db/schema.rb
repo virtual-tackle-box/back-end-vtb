d95506ef79ad83f73494f2ec918be26cb5f61faf
Wil Fady <wrfady@gmail.com>
2023-10-21 17:52:11 -0400

    Adds cloudinary urls to catches table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_213049) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_215130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_213049) do
     t.float "latitude"
     t.float "longitude"
     t.string "lure"
+    t.string "cloudinary_urls"
     t.index ["user_id"], name: "index_catches_on_user_id"
   end
 
