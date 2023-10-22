3c8dff762caa3ea63651bec17d17869a580a2d15
Wil Fady <wrfady@gmail.com>
2023-10-22 11:05:54 -0400

    Adds custom getter and setter methods for cloudinary urls in catch mdoel

--- app/models/catch.rb
+++ app/models/catch.rb
@@ -5,4 +5,16 @@ class Catch < ApplicationRecord
   validates :longitude, presence: true
   
   belongs_to :user
+
+    # Custom getter to deserialize cloudinary_urls
+    def cloudinary_urls
+      raw_value = read_attribute(:cloudinary_urls)
+      JSON.parse(raw_value) if raw_value.present?
+    end
+  
+    # Custom setter to serialize cloudinary_urls
+    def cloudinary_urls=(values)
+      serialized = values.to_json
+      write_attribute(:cloudinary_urls, serialized)
+    end
 end
